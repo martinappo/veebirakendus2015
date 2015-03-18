53130d8ccf462cafb81b1800b48dc3d6e2544a39
martinappo <martinappo@gmail.com>
2015-03-18 18:37:40 +0200

    More comments.

--- app/Tag.php
+++ app/Tag.php
@@ -4,9 +4,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model {
 
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'tags';
+
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
 	protected $fillable = [
 		'name'
 	];
+
 	/**
 	 * Get the trainings associated with the given tag 
 	 * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
--- app/Training.php
+++ app/Training.php
@@ -4,8 +4,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Training extends Model {
 
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
 	protected $table = 'trainings';
 
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
 	protected $fillable = [
 		'aadress',
 		'title',
