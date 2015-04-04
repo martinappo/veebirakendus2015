342d3380a291e3248ad669ce87de765ebb006742
martinappo <martinappo@gmail.com>
2015-04-04 18:49:52 +0300

    User interface fixes.

--- resources/views/trainings/index.blade.php
+++ resources/views/trainings/index.blade.php
@@ -5,14 +5,19 @@
 		<h1>Treeningud</h1>
 	</div>
 
-	<div class="col-md-6">
+	<div class="col-md-12">
 		<div id="map-container" class="map-container"></div>
 	</div>
+	<div class="clearfix"></div>
+	<hr>
 	<div class="col-md-6">
 		{!! Form::open(['method' => 'GET', 'url' => 'trainings/search', 'id' => 'search-form']) !!}
 			<div class="form-group">
-				{!! Form::label('tag_list', 'Märksõnad') !!}
+				{!! Form::label('tag_list', 'Otsi märksõna järgi') !!}
 				{!! Form::select('tag_list[]', $tags, null, ['id' => 'tag_list', 'class' => 'form-control', 'multiple']) !!}
+				<div class="small">
+					Otsitakse treeningukoha nime, kirjelduse ja märksõnade hulgast.
+				</div>
 			</div>
 			<div class="form-group">
 				{!! Form::submit('Otsi' , ['class' => 'btn btn-primary', 'id' => 'search-trainings']) !!}
